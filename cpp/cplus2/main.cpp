03835a2543d3a52db55c293a9c6c595adbf5001d
Am@n Chouh@n <amanchouhan192@gmail.com>
2018-03-22 18:48:07 +0530

    initial commit \/( * _ *)\/......

---
+++ cpp/cplus2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, m;
+    // Read the two numbers from STDIN
+    cin >> n >> m;
+    // Store the difference between them in result
+    int result = n - m;
+
+    // If result is negative multiply it with -1
+    if(result < 0) {
+        result = result*(-1);
+    }
+
+    // Print the result to STDOUT
+    cout << result;
+    return 0;
+}
